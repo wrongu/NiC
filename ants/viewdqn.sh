d5c71941b966577adb793c013b86e2cd70d9ac03
wrongu <lange.richard.d@gmail.com>
2015-03-13 14:10:37 -0400

    adding viewdqn.sh to do playback

---
+++ ants/viewdqn.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+NET=$1
+EP=$2
+TURNS=60
+
+python tools/playgame.py "dqnbot/torch/bin/luajit DQNBot.lua -resume -test -testing_ep "$EP" -save_name "$NET" -nturns "$TURNS"" "python tools/sample_bots/python/HunterBot.py" --map_file tools/maps/example/tutorial1.map --log_dir game_logs --turns $TURNS --scenario --food none --player_seed 7 --verbose -e
