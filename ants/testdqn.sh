99c6e62fc956a4bafce24233ae6a72375315b954
RICHARD DEVENS LANGE <rlange@bluehive.circ.rochester.edu>
2015-03-14 15:16:46 -0400

    fixed loop in testdqn

--- ants/testdqn.sh
+++ ants/testdqn.sh
@@ -3,7 +3,7 @@
 TURNS=1000
 START_GENERATION=$1
 END_GENERATION=$2
-PREFIX="hunter_rand"
+PREFIX=$3
 
 for GEN in `seq $START_GENERATION $END_GENERATION`
 do
@@ -11,15 +11,16 @@ do
 	TRIES=0
 	# Note that if it doesn't reach save_freq turns, no network is saved.
 	# So, repeat until it works:
-	while [ ! -e "dqnbot/saved_networks/$FILENAME" ] && [ $TRIES -lt 5 ]
+	while [ ! -e "dqnbot/saved_networks/$FILENAME.t7" ] && [ $TRIES -lt 5 ]
 	do
+		echo "attempt $TRIES"
 		echo "python tools/playgame.py \"dqnbot/torch/bin/luajit DQNBot.lua -resume -save_freq 100 -save_name "$FILENAME" -nturns "$TURNS"\" \"python tools/sample_bots/python/HunterBot.py\" --nolaunch --map_file tools/maps/example/tutorial1.map --log_dir game_logs --turns $TURNS --scenario --food none --player_seed 7 --verbose -e"	
 		python tools/playgame.py "dqnbot/torch/bin/luajit DQNBot.lua -resume -save_freq 100 -save_name "$FILENAME" -nturns "$TURNS"" "python tools/sample_bots/python/HunterBot.py" --nolaunch --map_file tools/maps/example/tutorial1.map --log_dir game_logs --turns $TURNS --scenario --food none --player_seed 7 --verbose -e
 		let TRIES=TRIES+1
 	done
 
 	# if still unsuccessful, just quit
-	if [ ! -e "dqnbot/saved_networks/$FILENAME" ]
+	if [ ! -e "dqnbot/saved_networks/$FILENAME.t7" ]
 	then
 		break;
 	fi
