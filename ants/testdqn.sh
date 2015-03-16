72879e44973cc44422887525219fabfe63525c93
RICHARD DEVENS LANGE <rlange@bluehive.circ.rochester.edu>
2015-03-16 17:21:47 -0400

    time limit extended

--- ants/testdqn.sh
+++ ants/testdqn.sh
@@ -14,8 +14,8 @@ do
 	while [ ! -e "dqnbot/saved_networks/$FILENAME.t7" ] && [ $TRIES -lt 30 ]
 	do
 		echo "attempt $TRIES"
-		echo "python tools/playgame.py \"dqnbot/torch/bin/luajit DQNBot.lua -resume -save_freq 100 -save_name "$FILENAME" -nturns "$TURNS"\" \"python tools/sample_bots/python/HunterBot.py\" --nolaunch --map_file tools/maps/example/tutorial1.map --log_dir game_logs --turns $TURNS --scenario --food none --player_seed 7 --verbose -e"	
-		python tools/playgame.py "dqnbot/torch/bin/luajit DQNBot.lua -resume -save_freq 100 -save_name "$FILENAME" -nturns "$TURNS"" "python tools/sample_bots/python/HunterBot.py" --nolaunch --map_file tools/maps/example/tutorial1.map --log_dir game_logs --turns $TURNS --scenario --food none --player_seed 7 --verbose -e
+		echo "python tools/playgame.py \"dqnbot/torch/bin/luajit DQNBot.lua -resume -save_freq 100 -save_name "$FILENAME" -nturns "$TURNS"\" \"python tools/sample_bots/python/HunterBot.py\" --nolaunch --turntime 5000 --map_file tools/maps/example/tutorial1.map --log_dir game_logs --turns $TURNS --scenario --food none --player_seed 7 --verbose -e"	
+		python tools/playgame.py "dqnbot/torch/bin/luajit DQNBot.lua -resume -save_freq 100 -save_name "$FILENAME" -nturns "$TURNS"" "python tools/sample_bots/python/HunterBot.py" --nolaunch --turntime 5000 --map_file tools/maps/example/tutorial1.map --log_dir game_logs --turns $TURNS --scenario --food none --player_seed 7 --verbose -e
 		let TRIES=TRIES+1
 	done
 
--- ants/viewdqn.sh
+++ ants/viewdqn.sh
@@ -3,4 +3,4 @@ NET=$1
 EP=$2
 TURNS=60
 
-python tools/playgame.py "dqnbot/torch/bin/luajit DQNBot.lua -resume -test -testing_ep "$EP" -save_name "$NET" -nturns "$TURNS"" "python tools/sample_bots/python/HunterBot.py" --map_file tools/maps/example/tutorial1.map --log_dir game_logs --turns $TURNS --scenario --food none --player_seed 7 --verbose -e
+python tools/playgame.py "dqnbot/torch/bin/luajit DQNBot.lua -resume -test -testing_ep "$EP" -save_name "$NET" -nturns "$TURNS"" "python tools/sample_bots/python/HunterBot.py" --map_file tools/maps/example/tutorial1.map --turntime 5000 --log_dir game_logs --turns $TURNS --scenario --food none --player_seed 7 --verbose -e
